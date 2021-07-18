9110b278f93c97301afa4c49638ebc89ffd7136b
Patrik Sehnoutek <patriksehnoutek@gmail.com>
2021-07-18 19:46:33 +0200

    Add cog listener
    New feature which send message and gif when a specific user is mentioned

--- cogs/jokes.py
+++ cogs/jokes.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 import random
+import os
+from os.path import dirname, join
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+CHOSEN = os.getenv('CHOSEN')
 
 
 class Jokes(commands.Cog):
@@ -19,6 +26,16 @@ class Jokes(commands.Cog):
     @commands.command()
     async def little(self, ctx):
         await ctx.send(":pinching_hand:")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        mentioned = message.mentions
+
+        for user in mentioned:
+            if user.id == int(CHOSEN):
+                await message.channel.send(f'**{user.name}** má rád **VEĽKÉ kravy**.')
+                path = join(dirname(__file__), 'krava.gif')
+                await message.channel.send(file=discord.File(path))
 
 
 def setup(bot):
