522da69559d664999fd7eb4866c3ea9e459111c0
Patrik Sehnoutek <patriksehnoutek@gmail.com>
2021-07-18 17:01:26 +0200

    Replace setup function from every cog with extensions

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import os
-import cogs.greetings as cg
-import cogs.jokes as jk
-
 
 # Load environment variables
 load_dotenv()
@@ -12,24 +9,13 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # Bot commands
 bot = commands.Bot(
-    command_prefix="$",
+    command_prefix=["$", "?"],
     case_insensitive=True
 )
 
-cg.setup(bot)
-jk.setup(bot)
-
-
-"""
-cog = bot.get_cog('Greetings')
-commands = cog.get_commands()
-print([c.name for c in commands])
-"""
-
-
-@bot.command()
-async def pomoc(ctx):
-    await ctx.send("Help yourself. Irony :pinching_hand:")
-
+# Extent bot functionality with cogs
+extensions = ['greetings', 'jokes']
+for extension in extensions:
+    bot.load_extension(f"cogs.{extension}")
 
 bot.run(BOT_TOKEN)
